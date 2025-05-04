39a98e8758101f506a01b69e4af9e71e0adb6a9d
Jokingpanda69 <113431699+Jokingpanda69@users.noreply.github.com>
2025-05-04 23:22:51 +0530

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -33,27 +33,39 @@ void parallel_bubble_sort(int n, vector <int>&arr)
     }
 }
 
-int main()
-{
-    int n = 1000;
-    srand(time(0));
-    vector<int> arr1,arr2;
-    for (int i = 0; i < n; i++)
-    {
-        int val=rand() % n + 1;
-        arr1.push_back(val);
-        arr2.push_back(val);
+
+int main() {
+    srand(time(0)); // Seed for random number generator
+
+    int n;
+    cout << "Enter number of random elements to generate: ";
+    cin >> n;
+
+    vector<int> arr1(n), arr2(n);
+
+    for (int i = 0; i < n; ++i) {
+        int val = rand() % 100 + 1; // Random numbers between 1 and 100
+        arr1[i] = val;
+        arr2[i] = val;
     }
 
+    cout << "\nRandomly generated array:\n";
+    for (int val : arr1) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    // Sequential Bubble Sort
     double start1 = omp_get_wtime();
     bubble_sort(n, arr1);
     double end1 = omp_get_wtime();
-    cout << "\nSequential time taken :" << (end1 - start1) * 1000 << "ms" << endl;
+    cout << "\nSequential Bubble Sort Time: " << (end1 - start1) * 1000 << " ms" << endl;
 
+    // Parallel Bubble Sort
     double start2 = omp_get_wtime();
     parallel_bubble_sort(n, arr2);
     double end2 = omp_get_wtime();
-    cout << "\nParallel time taken :" << (end2 - start2) * 1000 << "ms" << endl;
+    cout << "Parallel Bubble Sort Time: " << (end2 - start2) * 1000 << " ms" << endl;
 
     return 0;
 }
